88b9dd77d527951cb57e8bf13008bc6c75576b24
Jose Carlos Junior <josecarlosjr.atc@gmail.com>
2024-07-26 10:44:11 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/atccorp/SpringBootProject/config/TestConfig.java
+++ src/main/java/com/atccorp/SpringBootProject/config/TestConfig.java
@@ -52,6 +52,15 @@ public class TestConfig implements CommandLineRunner {
 		categoryRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5));	
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		p1.getCategories().add(c3);
+		p2.getCategories().add(c1);
+		p2.getCategories().add(c2);
+		p3.getCategories().add(c4);
+		p4.getCategories().add(c5);
+		p5.getCategories().add(c3);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		User u3 = new User(null, "Eric Rian", "ericr@gmail.com", "856935856", "123456");
